ef34a83e98310963d9f658d088c5ebd6edb68d27
guatambu <guatambu@dundak.com>
2018-06-28 11:34:51 -0700

    Initial Commit

---
+++ SWAPIDevMountainObj-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWAPIDevMountainObj-C
+//
+//  Created by Kelly Johnson on 6/28/18.
+//  Copyright © 2018 Leme Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SWAPIDevMountainObj-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SWAPIDevMountainObj-C
+//
+//  Created by Kelly Johnson on 6/28/18.
+//  Copyright © 2018 Leme Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
